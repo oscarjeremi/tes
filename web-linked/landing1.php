cb4e3f69bc3e81381ce65c1cc23ff93aab053de2
oscarjeremi <oscarjeremi7@gmail.com>
2022-05-12 03:37:32 +0700

    web-link

---
+++ web-linked/landing1.php
@@ -0,0 +1,225 @@
+<?php session_start(); /* Starts the session */
+
+/* Check Login form submitted */
+if (isset($_POST['Submit'])) {
+  /* Define username and associated password array */
+  $logins = array('crm321' => "password1");
+
+  /* Check and assign submitted Username and Password to new variable */
+  // $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+  $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+  /* Check Username and Password existence in defined array */
+  if (isset($logins[$Password])) {
+    /* Success: Set session variables and redirect to Protected page  */
+    $_SESSION['UserData']['Password'] = $logins[$Password];
+    header("location:landing2.php");
+    exit;
+  } else {
+    /*Unsuccessful attempt: Set error message */
+    $msg = "<span >Invalid Login Details</span>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>List Sheet</title>
+  <link rel="shortcut icon" href="image/png/favicon.png" type="image/x-icon">
+  <!-- Bootstrap , fonts & icons  -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="fonts/icon-font/css/style.css">
+  <link rel="stylesheet" href="fonts/typography-font/typo.css">
+  <link rel="stylesheet" href="fonts/fontawesome-5/css/all.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- Plugin'stylesheets  -->
+  <link rel="stylesheet" href="plugins/aos/aos.min.css">
+  <link rel="stylesheet" href="plugins/fancybox/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="plugins/nice-select/nice-select.min.css">
+  <link rel="stylesheet" href="plugins/slick/slick.min.css">
+  <!-- Vendor stylesheets  -->
+  <link rel="stylesheet" href="css/settings.css">
+  <link rel="stylesheet" href="css/landing.css">
+  <!-- Custom stylesheet -->
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="icon" href="image/png/telkom_idn.png">
+
+</head>
+
+<body>
+  <div id="loading">
+    <div class="load-circle"><span class="one"></span></div>
+  </div>
+  <div class="site-wrapper overflow-hidden dark-theme">
+    <header class="site-header header-8 bg--conflower-blue sticky-header">
+      <div class="container-fluid pr-lg--30 pl-lg--30">
+        <nav class="navbar site-navbar offcanvas-active navbar-expand-lg navbar-light mb--30">
+          <!-- Brand Logo-->
+          <div class="container-fluid"><a class="navbar-brand" style="color: #ECECEC;" href="">
+              <h3>CRM</h3>
+            </a></div>
+          <div class="collapse navbar-collapse" id="mobile-menu">
+            <div class="navbar-nav ml-lg-auto mr--10">
+              <button class="btn-close navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="true" aria-label="Toggle navigation">
+                <i class="icon icon-simple-remove"></i>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- Service Section -->
+    <div class="omga-08__service-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="section-title text-center">
+              <h2 data-aos="fade-up" data-aos-duration="2000" data-aos-once="true" data-aos-delay="2000">Linked Spreadsheet
+                <span class="text--primary">(Open)</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-lg--45">
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="800" data-aos-once="true">
+            <a href="https://docs.google.com/spreadsheets/d/1CdC9fWhZEye_u0XXtjhD8Ej4B4kfPisz9IeR46WVmV0/edit?usp=sharing" target="_blank" class="service-card--1">
+              <h3 class="card-title">Close the Loop</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="200">
+            <a href="https://docs.google.com/spreadsheets/d/1h_VFW4YJmNrKXtOibdZkdM0neHdRBgiMx3QDKmO_4bQ/edit?usp=sharing" target="_blank" class="service-card--1">
+              <h3 class="card-title">Program HVC</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="400">
+            <a href="https://docs.google.com/spreadsheets/d/14zYNNu0ih1rQGH6F0rAUxCUITL7sm_htcURcGqY_Yms/edit?usp=sharing" target="_blank" class="service-card--1">
+              <h3 class="card-title">Orbit TR3</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="600">
+            <a href="https://docs.google.com/spreadsheets/d/16x1IfBWtrbPTOxkcaGp4W5ROgiG7MoNCC02UjSYhQYw/edit?usp=sharing" target="_blank" class="service-card--1">
+              <h3 class="card-title">Rekap Pertanggungan CX Commander</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="800">
+            <a href="https://docs.google.com/spreadsheets/d/13joZJKipC1sOA3YMnRhfJw9lChNJAtExYkmLhQNq5WM/edit?usp=sharing" target="_blank" class="service-card--1">
+              <h3 class="card-title">Penukaran Poin Plasa TR3</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+          <div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="200">
+            <a href="https://docs.google.com/spreadsheets/d/1YlTrKEKlOqU5ghQvnRqO3dOx2ctyPXxvx-syKapHdFk/edit#gid=849919322" target="_blank" class="service-card--1">
+              <h3 class="card-title">Indihome Barakah</h3>
+              <i class="fas fa-chevron-circle-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Case Study Section -->
+    <div class="omga-08__case-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="section-title text-center">
+              <h2 data-aos="fade-up" data-aos-duration="800" data-aos-once="true" data-aos-delay="500">Linked Spreadsheet
+                <span class="text--primary">(Private)</span>
+              </h2>
+            </div>
+          </div>
+          <div class="col-lg-5 col-md-5 mb--30 mt--30 " data-aos="fade-up" data-aos-duration="800" data-aos-delay="500" data-aos-once="true">
+            <div class="case-card--1">
+              <div class="c-card-img">
+                <img src="image/png/telkom_idn (1).png" alt="">
+                <a href="" class="cc-btn btn--primary" data-bs-toggle="modal" data-bs-target="#modalku">View</a>
+              </div>
+              <div class="c-card-text">
+                <div class="shape">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 0 540 22">
+                    <g>
+                      <g clip-path="url(#clip-9C6BB70E-720F-46FC-BAD4-9064EA06E219)">
+                        <path d="M0 0s233.088 28.458 539.999 0c306.91-28.458 0 22 0 22H.06" />
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+                <h3 class="c-title">Stock Sovenir, Pertanggungan CRM, Proker CRM TR3.</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer-section footer-1 bg-indigo omga-footer--8">
+        <div class="copyright-area">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-sm-6  text-sm-left text-center mt-2 mb-sm-0">
+                <label for="">
+                  <p>@CRM3 2022</p>
+                </label>
+              </div>
+              <div class="col-sm-6 text-sm-right text-center">
+                <ul class="social-icons">
+                  <li><a href="https://twitter.com/TelkomJabar"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="https://www.linkedin.com/company/telekomunikasi-indonesia/mycompany/"><i class="fab fa-linkedin"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--Modal Body-->
+  <div class="modal" tabindex="-1" id="modalku">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" style="font-weight:bold">Password</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="" method="post" name="Login_Form">
+          <div class="modal-body">
+            <?php if (isset($msg)) { ?>
+              <div class="alert alert-danger" role="alert"><?= $msg; ?></div>
+            <?php } ?>
+            <td>
+              <input style="width: 20rem; justify-content: center;" type="password" name="Password" class="Input">
+            </td>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" name="Submit" value="Login" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Vendor Scripts -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="plugins/jquery/jquery-migrate.min.js"></script>
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Plugin's Scripts -->
+  <script src="plugins/fancybox/jquery.fancybox.min.js"></script>
+  <script src="plugins/nice-select/jquery.nice-select.min.js"></script>
+  <script src="plugins/aos/aos.min.js"></script>
+  <script src="plugins/slick/slick.min.js"></script>
+  <!-- Activation Script -->
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ web-linked/landing2.php
@@ -0,0 +1,131 @@
+<?php session_start(); /* Starts the session */
+
+if (!isset($_SESSION['UserData']['Password'])) {
+	header("location:landing1.php");
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>List Private Sheet</title>
+	<link rel="shortcut icon" href="image/png/favicon.png" type="image/x-icon">
+	<!-- Bootstrap , fonts & icons  -->
+	<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="fonts/icon-font/css/style.css">
+	<link rel="stylesheet" href="fonts/typography-font/typo.css">
+	<link rel="stylesheet" href="fonts/fontawesome-5/css/all.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<!-- Plugin'stylesheets  -->
+	<link rel="stylesheet" href="plugins/aos/aos.min.css">
+	<link rel="stylesheet" href="plugins/fancybox/jquery.fancybox.min.css">
+	<link rel="stylesheet" href="plugins/nice-select/nice-select.min.css">
+	<link rel="stylesheet" href="plugins/slick/slick.min.css">
+	<!-- Vendor stylesheets  -->
+	<link rel="stylesheet" href="css/settings.css">
+	<link rel="stylesheet" href="css/landing.css">
+	<!-- Custom stylesheet -->
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="icon" href="image/png/telkom_idn.png">
+
+</head>
+
+<body>
+	<div id="loading">
+		<div class="load-circle"><span class="one"></span></div>
+	</div>
+	<div class="site-wrapper overflow-hidden dark-theme">
+		<!-- Header Area -->
+		<header class="site-header header-8 bg--conflower-blue sticky-header">
+			<div class="container-fluid pr-lg--30 pl-lg--30">
+				<nav class="navbar site-navbar offcanvas-active navbar-expand-lg navbar-light mb--30">
+					<!-- Brand Logo-->
+					<div class="container-fluid"><a class="navbar-brand" style="color: #ECECEC;" href=""><h3>CRM</h3></a></div>
+					<div class="collapse navbar-collapse" id="mobile-menu">
+						<div class="navbar-nav ml-lg-auto mr--10">
+							<button class="btn-close navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="true" aria-label="Toggle navigation">
+								<i class="icon icon-simple-remove"></i>
+							</button>
+						</div>
+					</div>
+					<div class="header-btns ml-auto ml-lg-0"><a class="btn btn--primary btn-header hvr-bounce-to-left goto" href="logout.php">Kembali</a></div>
+					<button class="navbar-toggler btn-close-off-canvas toggler-white" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
+						<i class="icon icon-simple-remove icon-close"></i>
+						<i class="icon icon-menu-34 icon-burger"></i>
+					</button>
+				</nav>
+			</div>
+		</header>
+		<!-- Service Section -->
+		<div class="omga-08__service-section">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-10">
+						<div class="section-title text-center">
+							<h2 data-aos="fade-up" data-aos-duration="2000" data-aos-once="true" data-aos-delay="2000">Linked Spreadsheet
+								<span class="text--primary">(Private)</span>
+							</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-lg--45">
+					<div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="800" data-aos-once="true">
+						<a href="https://docs.google.com/spreadsheets/d/1A99u8G21HWJKWES-ITkutwgOjCIyVzh80Pa8uCzNHnQ/edit?usp=sharing" target="_blank" class="service-card--1">
+							<h3 class="card-title">Stock Sovenir</h3>
+							<i class="fas fa-chevron-circle-right"></i>
+						</a>
+					</div>
+					<div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="200">
+						<a href="https://docs.google.com/spreadsheets/d/1cSC7pMg3bS7VyS6VUHwDumAH_XyMDC5rHr0-AGyusQo/edit?usp=sharing" target="_blank" class="service-card--1">
+							<h3 class="card-title">Pertanggungan CRM</h3>
+							<i class="fas fa-chevron-circle-right"></i>
+						</a>
+					</div>
+					<div class="col-lg-4 mt--30" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" data-aos-delay="400">
+						<a href="https://docs.google.com/spreadsheets/d/1ZbtGhViW9zJ4WRQLufR7pEcEBqDYWnM0WgXvB0cGw0g/edit?usp=sharing" target="_blank" class="service-card--1">
+							<h3 class="card-title">Proker CRM TR3</h3>
+							<i class="fas fa-chevron-circle-right"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-fixed">
+			<div class="footer-section footer-1 bg-indigo omga-footer--8">
+				<div class="copyright-area">
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-sm-6  text-sm-left text-center mt-2 mb-sm-0">
+								<label for="">
+									<p>@CRM3 2022</p>
+								</label>
+							</div>
+							<div class="col-sm-6 text-sm-right text-center">
+								<ul class="social-icons">
+									<li><a href="https://twitter.com/TelkomJabar"><i class="fab fa-twitter"></i></a></li>
+									<li><a href="https://www.linkedin.com/company/telekomunikasi-indonesia/mycompany/"><i class="fab fa-linkedin"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Vendor Scripts -->
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<script src="plugins/jquery/jquery-migrate.min.js"></script>
+	<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Plugin's Scripts -->
+	<script src="plugins/fancybox/jquery.fancybox.min.js"></script>
+	<script src="plugins/nice-select/jquery.nice-select.min.js"></script>
+	<script src="plugins/aos/aos.min.js"></script>
+	<script src="plugins/slick/slick.min.js"></script>
+	<!-- Activation Script -->
+	<script src="js/custom.js"></script>
+</body>
